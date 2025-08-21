eafa65a6e21695b29cd96b0688a7b442b2b65f54
umberto <umbertofarsetti@gmail.com>
2025-08-21 16:00:45 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'prod',
     httpOnly: true,
     maxAge: 60 * 60 * 1000, // 1 hour
     sameSite: "none"
