661e5dbe3d4be01d30a938238f84d3319968b832
umberto <umbertofarsetti@gmail.com>
2025-08-21 16:05:52 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ import FileStore from 'session-file-store';
 import favicon from 'serve-favicon';
 import { Request, Response, NextFunction } from 'express';
 import { BASE_DIR } from '@config/paths';
-
 import indexRouter from '@routes/index';
 
 const app = express();
